6599cdfdceb2cf89dc65c1e23ea6a559b1dea398
sturzaveaceslav <email@example.comslavunea90@gmail.com>
2025-03-03 12:28:31 +0200

    modifica test2

--- src/test2/test2.java
+++ src/test2/test2.java
@@ -1,18 +1,25 @@
 package test2;
 
 public class test2 {
+public int a = 2;
 
+test2() {
 
-  public int a = 2;
+}
+test2(int b) {
+    a = b;
+}
+public int getA() {
+    System.out.println(a);
+    return a;
+}
+}
 
-    void getValuie() {
-       System.out.println(a);
+class secondClass {
+    public static void main(String[] args) {
+        test2 test = new test2();
+
+        test2 secondVariable = new test2(6);
+        test.getA();
     }
-    int getValue() {
-        return a;
-    }
-    void setValue(int b) {
-          a = b;
-    }
-
 }
