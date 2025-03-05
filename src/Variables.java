23326b2a2d6c38e68e2c8c4063a73618985b2fef
sturzaveaceslav <email@example.comslavunea90@gmail.com>
2025-03-05 22:57:33 +0200

    Creaza clasa Variables

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        long a = 7811080815L;
+        int b = 55235358;
+        char c1 = 65;
+        String c2 = "First Leter in Alphabet - ";
+        boolean d = true;
+        String d1 = "Java is Amazing - ";
+        double e = Math.PI;
+
+
+
+        System.out.println("Current Wordl Population - " + a);
+        System.out.println("Cars Producet This Year - " + b);
+        System.out.println(c2 + c1);
+        System.out.println(d1 + d);
+        System.out.println("Pi - " + e);
+    }
+}
--- src/persoana1/Person.java
+++ src/persoana1/Person.java
@@ -3,14 +3,9 @@ package persoana1;
 public class Person {
     public static void main(String[] args) {
 
-        Integer x=new Integer(5);
-        int y = 5+2;
-        System.out.println(x + "tare");
-        System.out.println(y);
-
-        String a = new String("abc");
-        String b = a.substring(0, 1);
-        System.out.println(a + "vs." + b);
+        int a = 25;
+        a = a++ + a + a-- - a-- + ++a;
+        System.out.println(a);
 
     }
 }
