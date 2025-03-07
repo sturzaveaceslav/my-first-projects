b031763b70ff358c4aa20cbc1bf013f5064716a0
sturzaveaceslav <email@example.comslavunea90@gmail.com>
2025-03-07 14:26:43 +0200

    Aditional homework 5

---
+++ src/homework/lesson4/CalculArea.java
@@ -0,0 +1,11 @@
+package homework.lesson4;
+
+public class CalculArea {
+    public static void main(String[] args) {
+        double area;
+        double pi = 3.14;
+        int radius = 35;
+        area = pi * Math.pow(radius, 2);
+        System.out.println(area);
+    }
+}
