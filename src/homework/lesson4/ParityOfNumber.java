76429addfc4f5126633c9515e26b589ca24b3a30
sturzaveaceslav <email@example.comslavunea90@gmail.com>
2025-03-07 13:49:20 +0200

    Aditional homework pc.4

---
+++ src/homework/lesson4/ParityOfNumber.java
@@ -0,0 +1,14 @@
+package homework.lesson4;
+
+public class ParityOfNumber {
+    public static void main(String[] args) {
+        int number = 3;
+        if (number % 2 == 0) {
+            System.out.println(number + " is even");
+
+        } else {
+            System.out.println(number + " is odd");
+        }
+
+    }
+}
